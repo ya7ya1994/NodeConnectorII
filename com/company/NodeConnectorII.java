f2f211a1f6cb475fff67956c5a072292601bb955
Yahya Abdul-Hakeem <42775710+ya7ya1994@users.noreply.github.com>
2020-03-10 18:23:44 -0700

    Update NodeConnectorII.java

--- com/company/NodeConnectorII.java
+++ com/company/NodeConnectorII.java
@@ -2,23 +2,42 @@ package com.company;
 
 public class NodeConnectorII {
     public Node connect(Node root) {
-        Node leftNode = root;
-        while (leftNode != null && leftNode.left != null) {
-            connector(leftNode);
-            leftNode = leftNode.left;
+        if(root == null) return root;
+        Node head = null;
+        Node prev = null;
+        Node curr = root;
+        
+        while(curr != null){
+            while(curr != null){
+                if(curr.left != null){
+                    if(head == null){
+                        head = curr.left;
+                        prev = curr.left;
+                    } else {
+                        prev.next = curr.left;
+                        prev = prev.next;
+                    }
+                }
+                
+                if(curr.right != null){
+                    if(head == null){
+                        head = curr.right;
+                        prev = curr.right;
+                    } else {
+                        prev.next = curr.right;
+                        prev = prev.next;
+                    }
+                }
+                
+                curr = curr.next;
+            }
+            
+            curr = head;
+            prev = null;
+            head = null;
         }
+        
         return root;
-    }
-
-    public void connector(Node leftNode) {
-        Node move = leftNode;
-        while (move != null) {
-            if (move.left != null)
-                move.left.next = move.right;
-            if (move.next != null)
-                move.right.next = move.next.right;
-            move = move.next;
-        }
     }
 }
 
